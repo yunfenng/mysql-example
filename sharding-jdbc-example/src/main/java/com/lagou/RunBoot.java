6c28c8182f2e58ecd03426e51834955b45e34d09
fenng <xqjia123@gmail.com>
2023-02-23 23:23:28 +0800

    sharding transaction

--- sharding-jdbc-example/src/main/java/com/lagou/RunBoot.java
+++ sharding-jdbc-example/src/main/java/com/lagou/RunBoot.java
@@ -1,12 +1,14 @@
 package com.lagou;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @Author: Jaa
  * @Date: 2023/2/7 0:10
  * @Description:
  */
+@EnableTransactionManagement
 @SpringBootApplication
 public class RunBoot {
 }
